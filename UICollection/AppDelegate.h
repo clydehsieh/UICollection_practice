cab8def82752781b99b71db932ec129290cd5316
clydehsieh <clydehsieh0601@gmail.com>
2015-06-23 10:26:27 +0800

    Initial Commit

---
+++ UICollection/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UICollection
+//
+//  Created by Chin-Hui Hsieh  on 6/23/15.
+//  Copyright (c) 2015 Chin-Hui Hsieh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
